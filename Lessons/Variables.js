5e2f1539560456a1bd5a69a916b8512a70d0159d
Lauren <ls_garwood@yahoo.co.uk>
2022-04-18 14:16:06 +0100

    LG: variables

---
+++ Lessons/Variables.js
@@ -0,0 +1,13 @@
+//Variables
+
+var name1 = "Maria Jones";
+var age = 21;
+var hasDriversLicence = true;
+var empty = undefined; //nothing inside of variable
+
+console.log(name1 + " = " + typeof name1);
+console.log(age +" = " + typeof age);
+console.log(hasDriversLicence + " = " +typeof hasDriversLicence);
+console.log(empty + " = " +typeof empty);
+
+
