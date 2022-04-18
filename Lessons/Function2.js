724fb077e196ee95e30740fd8d4e49c9f19fa9cc
Lauren <ls_garwood@yahoo.co.uk>
2022-04-18 20:43:51 +0100

    LG: functions2

---
+++ Lessons/Function2.js
@@ -0,0 +1,13 @@
+//More funtions - using built in function
+
+var person = {name: "Anna", age: 20};
+console.log(Object.keys(person));
+console.log(Object.values(person));
+
+console.log(typeof "James Bond");
+console.log("James Bond".toUpperCase());
+console.log("James Bond".toLowerCase());
+console.log("James Bond".endsWith("Bond"));
+console.log("James Bond".startsWith("Bond"));
+
+//....underscore.js good library of functions online
