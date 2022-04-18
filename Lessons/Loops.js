6e57ae083e58ef96bec9580283ef366eac7dbbc3
Lauren <ls_garwood@yahoo.co.uk>
2022-04-18 21:23:46 +0100

    LG: loops

---
+++ Lessons/Loops.js
@@ -0,0 +1,26 @@
+//Loops
+// initialise, when to stop, to increment;
+
+var persons = [
+    {name: "Alex", age: 22},
+    {name: "Maria", age: 30}
+]
+
+// for (var i = 0; i <= persons.length; i++) {
+//     console.log(persons[i].name);
+//     console.log(persons[i].age);
+//     console.log("----------------");
+// }
+
+var  number = 0;
+
+while(number < 5) {
+    number++;
+
+    if (number < 5) {
+        continue
+    }
+    console.log(number);
+}
+
+
