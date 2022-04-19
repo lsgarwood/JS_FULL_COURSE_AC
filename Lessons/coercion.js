c70a3cee4b2eae3cdb1de1450ad485a10e428104
Lauren <ls_garwood@yahoo.co.uk>
2022-04-19 19:02:43 +0100

    LG: corecion2

--- Lessons/coercion.js
+++ Lessons/coercion.js
@@ -1,5 +1,6 @@
 // Equality without type coercion 
 
+
 console.log(typeof 0 + " " + typeof false);
 console.log(0 == false);
 
@@ -8,3 +9,18 @@ console.log("0" == false);
 
 console.log(typeof 1 + " " + typeof "1");
 console.log(1 == "1");
+// converting one type to another to make them 'equals'
+
+// Use triple eqals === for equality WITHOUT coersion
+console.log(typeof 0 + " " + typeof false);
+console.log(0 === false);
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" === false);
+
+console.log(typeof 1 + " " + typeof "1");
+console.log(1 === "1");
+
+// not triple equals !== also
+
+// better to use === to be more accurate
