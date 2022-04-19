6d9caf9060fff2036ec170b9433337f78252a51b
Lauren <ls_garwood@yahoo.co.uk>
2022-04-19 10:42:47 +0100

    LG: coercion1

---
+++ Lessons/coercion.js
@@ -0,0 +1,10 @@
+// Equality without type coercion 
+
+console.log(typeof 0 + " " + typeof false);
+console.log(0 == false);
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" == false);
+
+console.log(typeof 1 + " " + typeof "1");
+console.log(1 == "1");
