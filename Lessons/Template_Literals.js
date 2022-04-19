4e86cc0e9e95a9089e8f7197402497a01a389c2e
Lauren <ls_garwood@yahoo.co.uk>
2022-04-19 20:27:08 +0100

    LG: Template Literals

---
+++ Lessons/Template_Literals.js
@@ -0,0 +1,13 @@
+// Temperate Literals
+
+const name = "Anna";
+const country = "Canada";
+const age = 33;
+
+console.log("Name " + name + " Country " + country + " Age " + age);
+
+//T.L allows you a more elegant solution to join values and strings
+
+console.log(`Name ${name.length} Country ${country} Age ${age}`);
+
+// ^ can use functions within back ticks too (.length)
